2371f6ea4df33883a7d2fad9214af196ffd6eb25
huybuivuanh <huy_buivu12345@yahoo.ca>
2026-03-08 19:09:25 -0600

    fix embeded maps

--- app/components/home/OurLocationSection.tsx
+++ app/components/home/OurLocationSection.tsx
@@ -5,11 +5,8 @@ const ADDRESS = {
   city: "Prince Albert, SK S6V 2K2",
 };
 
-const MAP_EMBED_URL =
-  "https://www.google.com/maps/embed?q=1400+6+Ave+E,Prince+Albert,SK+S6V+2K2&z=15&ie=UTF8&iwloc=&output=embed";
-
 const GOOGLE_MAPS_LINK =
-  "https://www.google.com/maps/search/?api=1&query=1400+6+Ave+E,Prince+Albert,SK+S6V+2K2";
+  "https://www.google.com/maps?cid=11746207782635991944&g_mp=CiVnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLkdldFBsYWNlEAEYASAB&hl=en&gl=CA&source=embed";
 
 export default function OurLocationSection() {
   return (
@@ -45,11 +42,10 @@ export default function OurLocationSection() {
 
             <div className="relative aspect-[4/3] w-full overflow-hidden rounded-xl border border-stone-200 bg-stone-100">
               <iframe
-                title="Asian Le Restaurant location"
-                src={MAP_EMBED_URL}
-                width="100%"
-                height="100%"
-                className="absolute inset-0 border-0"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.9769899489197!2d-105.7397006232774!3d53.2003288722473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53017a65b9325e77%3A0xa302e9be85f27388!2sAsian%20Le!5e0!3m2!1sen!2sca!4v1773018326281!5m2!1sen!2sca"
+                width="600"
+                height="450"
+                style={{ border: "0" }}
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
